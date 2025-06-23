941c694be173b377d940a1e09419aaa86f11e4ad
Sriram Yerra <sriram.yerra@itspe.co.in>
2025-06-23 16:19:17 +0530

    June-23

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# June 23
+
 import pickle
 import cvzone
 import cv2
@@ -5,24 +7,74 @@ import numpy as np
 
 cap = cv2.VideoCapture('carPark.mp4')
 
-with open('/home/sriram/Documents/Live-Tracking-of-Parking-Space-using-ComputerVision-/CarParkPos', 'rb') as f:
+with open('CarParkPos', 'rb') as f:
+# with open('/home/sriram/Documents/Live-Tracking-of-Parking-Space-using-ComputerVision-/CarParkPos', 'rb') as f:
     posList = pickle.load(f)
 
 width, height = 107, 48
 
 def checkParkingSpace(imgPro):
+
+    spacecount = 0
+
     for pos in posList:
         x, y = pos
 
         imgCrop = imgPro[y:y+width, x:x+height]
         
-        # cv2.imshow('hi', imgCrop)
-        cv2.imshow(str(x*y), imgCrop) # fir making every image chunk unique.
+        ## cv2.imshow('hi', imgCrop)
+        # cv2.imshow(str(x*y), imgCrop) # fir making every image chunk unique.
 
         count = cv2.countNonZero(imgCrop)
         # used to count the number of white (non-zero) pixels in a binary image or image region.
 
-        # 50:21
+        if count < 900:
+            color = (255, 0, 0)
+            thickness = 3
+            spacecount += 1
+        else:
+            color = (0, 0, 255)
+            thickness = 2
+
+        cv2.rectangle(img, pos, (pos[0]+width, pos[1]+height), color, thickness)
+        cv2.putText(
+            img,
+            str(count),
+            (x, y + height - 3),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.9,
+            (0, 255, 0),
+            thickness
+        )
+
+        displayText = f"Free: {spacecount}/{len(posList)}"
+        cvzone.putTextRect(
+            img, 
+            displayText, 
+            (50, 50), 
+            scale=2,
+            thickness=5, 
+            offset=20, 
+            colorR=(0,0,0)
+        )
+
+        displayText = f"Red --> Occupied, Blue --> Free"
+        cvzone.putTextRect(
+            img, 
+            displayText, 
+            (400, 50), 
+            scale=2,
+            thickness=3, 
+            offset=20, 
+            colorR=(0, 0, 0)
+        )
+
+        cv2.imshow("finalImg", img)
+
+        # cv2.rectangle(imgPro, pos, (pos[0]+width, pos[1]+height), (255, 255, 255), thickness)
+        # cv2.imshow("Image", imgPro)
+
+
 
 # Making the video to run for an infinite loop. 
 while True:
@@ -36,7 +88,7 @@ while True:
 
     imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    imgBlur = cv2.GaussianBlur(imgGrey, (5, 5), 0)
+    imgBlur = cv2.GaussianBlur(imgGrey, (1, 1), 1)
     # (5,5) is the kernel size (width, height) of the filter.
     # It must be an odd number (like 3, 5, 7, 9...), a larger kernel = more blur.
     # 4 --> sigma (Standard Deviation)
@@ -48,7 +100,7 @@ while True:
     # where each pixel is either black or white, 
     
     # This below line applies median blur to a binary (thresholded) image in OpenCV:
-    imgThreshMedianBlur = cv2.medianBlur(imgThresh, 5)
+    imgThreshMedianBlur = cv2.medianBlur(imgThresh, 5) # --> 2nd parameter should be an odd number. 
     # To remove small noise (specks, dots, or jagged edges) from the binary 
     ## image imgThresh, while preserving the edges better than Gaussian blur.
 
@@ -58,7 +110,8 @@ while True:
     # # Larger kernel → stronger dilation.
     
     # cv2.dilate(), which is used to expand white regions in a binary image.
-    imgdilate = cv2.dilate(imgThreshMedianBlur, kernel, iterations = 1)
+    # imgdilate = cv2.dilate(imgThreshMedianBlur, kernel, iterations = 1)
+    imgdilate = cv2.dilate(imgThreshMedianBlur, kernel)
     # Helps to:
         #1 Fill small black holes/gaps inside white areas
         #2 Strengthen or connect lines or shapes
@@ -69,10 +122,12 @@ while True:
     checkParkingSpace(imgdilate)
 
     # Drawing the rectangle on the frame after the image chunks have been created
-    for pos in posList:
-        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (0, 255, 0), 2)
+    # for pos in posList:
+    #     cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (0, 255, 0), 2)
 
     # cv2.imshow("Image", imgThresh)
-    cv2.imshow("Image", imgThreshMedianBlur)
+    # cv2.imshow("Image", img)
+    # cv2.imshow("Image", imgdilate)
 
-    cv2.waitKey(1)
+    # cv2.waitKey(1)
+    cv2.waitKey(10)
